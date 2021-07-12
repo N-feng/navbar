e6186b184aad2fed1949857e30ddfe5d65ddb043
N-feng <308561157@qq.com>
2021-07-12 17:17:04 +0800

    feat: 优化navbar渲染逻辑

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,22 +1,43 @@
 <template>
-  <a-layout id="components-layout-side" style="min-height: 100vh">
-    <a-layout-sider v-model="collapsed" collapsible style="background: #fff">
-      <Menu />
-    </a-layout-sider>
-  </a-layout>
+  <nav>
+    <a-layout id="components-layout-side" style="min-height: 100vh">
+      <a-layout-sider collapsible style="background: #fff" @collapse="collapse">
+        <Menu :routes="routes" />
+      </a-layout-sider>
+    </a-layout>
+  </nav>
 </template>
 <script>
 import Menu from "./Menu.vue";
 
 export default {
   components: { Menu },
-  data() {
-    return {
-      collapsed: false
-    };
+  data: () => ({
+    collapsed: false,
+    routes: JSON.parse(localStorage.getItem("router")) || []
+  }),
+  methods: {
+    collapse(collapsed) {
+      window.myBus.emit("setCollapsed", collapsed);
+    }
   }
 };
 </script>
+
+<style scoped>
+nav {
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  /* css var comes from the styleguide */
+  width: var(--navbar-width);
+  background-color: white;
+  transition: left 0.2s ease-in-out;
+  box-shadow: 0.2rem 0 0.2rem var(--light-gray);
+  z-index: 1000;
+}
+</style>
 
 <style>
 #components-layout-side .ant-layout-sider-trigger {
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,27 +2,82 @@
   <a-menu mode="vertical" :style="{ lineHeight: '64px', height: '100%' }">
     <template v-for="item in routes">
       <a-menu-item v-if="!item.menus.length" :key="item.menuCode">
-        <div>
+        <div @click="handleLink(item)">
           <span>{{ item.menuName }}</span>
         </div>
       </a-menu-item>
-      <SubMenu v-else :key="item.menuCode" :menu-info="item" />
+      <SubMenu
+        v-else
+        :key="item.menuCode"
+        :menu-info="item"
+        @handleLink="handleLink"
+      />
     </template>
   </a-menu>
 </template>
 
 <script>
-import SubMenu from "./SubMenu.vue";
+import { Menu } from "ant-design-vue";
+
+const SubMenu = {
+  template: `
+    <a-sub-menu :key="menuInfo.menuCode" v-bind="$props" v-on="$listeners">
+      <span slot="title">
+        <a-icon type="mail" /><span>{{ menuInfo.menuName }}</span>
+      </span>
+      <template v-for="item in menuInfo.menus">
+        <a-menu-item v-if="!item.menus.length" :key="item.menuCode">
+          <div @click="$emit('handleLink', item)">
+            <a-icon type="pie-chart" />
+            <span>{{ item.menuName }}</span>
+          </div>
+        </a-menu-item>
+        <sub-menu v-else :key="item.menuCode" :menu-info="item" />
+      </template>
+    </a-sub-menu>
+  `,
+  name: "SubMenu",
+  // must add isSubMenu: true
+  isSubMenu: true,
+  props: {
+    ...Menu.SubMenu.props,
+    // Cannot overlap with properties within Menu.SubMenu.props
+    menuInfo: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+};
+
+/**
+ * @description 判读是否为外链
+ * @param path
+ * @returns {boolean}
+ */
+export function isExternal(path) {
+  return /^(https?:|mailto:|tel:)/.test(path);
+}
 
 export default {
   components: { SubMenu },
   props: {
-    githubLink: String
+    routes: {
+      type: Array,
+      default: () => []
+    }
   },
-  data() {
-    return {
-      routes: JSON.parse(localStorage.getItem("router") || "[]")
-    };
+  methods: {
+    handleLink(item) {
+      const routePath = item.url;
+      const target = item.target;
+      if (target === "_blank") {
+        if (isExternal(routePath)) window.open(routePath);
+        else if (this.$route.path !== routePath) window.open(routePath);
+      } else {
+        if (isExternal(routePath)) window.location.href = routePath;
+        else if (this.$route.path !== routePath) this.$router.push(routePath);
+      }
+    }
   }
 };
 </script>
